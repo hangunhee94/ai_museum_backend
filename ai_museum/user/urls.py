710ba2a1fa0ea7e90da6b0881c516bff4bf56a24
seonyoungbaek <rk4418308@gmail.com>
2022-07-04 19:11:36 +0900

    login, join complete

--- ai_museum/user/urls.py
+++ ai_museum/user/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from user import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
     # user/
     path('', views.UserView.as_view()),
-    path('login/', views.UserAPIView.as_view()),
-    path('logout/', views.UserAPIView.as_view()),
+    path('signup/', views.UserView.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
